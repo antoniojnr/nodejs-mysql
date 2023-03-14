57ddc4ede2d2e0d4d422deea5ad78446bc0c9832
Antonio Dias <addsjunior@gmail.com>
2023-03-14 16:32:52 -0300

    adding express

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var mysql = require("mysql");
 require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
 
-var con = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASS,
+app.get("/", (req, res) => {
+  var con = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+  });
+
+  con.connect(function (err) {
+    if (err) throw err;
+    res.send("Connected!");
+  });
 });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
